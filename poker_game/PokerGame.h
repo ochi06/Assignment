a8f4b3116436d84243be1b5da0e2f372b1c20570
Ochiai <ochiai.omu@gmail.com>
2025-06-10 14:55:38 +0900

    Update and rename report-3-AJG23001_pokergame.h to PokerGame.h

--- poker_game/PokerGame.h
+++ poker_game/PokerGame.h
@@ -1,9 +1,7 @@
-//AJG23001 落合梓香 メンバ関数、データメンバに関する知識が曖昧なところを自分で調べながら実装したので、良い復習になった。
-
 #include<stdlib.h>
 #include<stdio.h>
 #include <time.h>
-#include "report-3_poker.h"
+#include "Poker.h"
 
 class PokerGame : public Poker{
 
